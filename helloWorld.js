eb40796115a57a4cd486511c0c4a3464c24dd2a7
thuongtrinh <thuongtrinh@users.noreply.github.com>
2019-04-30 23:41:26 +0700

    print cat and mouse

--- helloWorld.js
+++ helloWorld.js
@@ -1,3 +1,10 @@
 
 var animal = "Tiger";
 console.log(animal);
+
+//Print detail of Mouse and Cat
+let mouse = new Mouse();
+let cat = new Cat();
+
+console.log(mouse.name + " - " + mouse.age);
+console.log(cat.printName);
